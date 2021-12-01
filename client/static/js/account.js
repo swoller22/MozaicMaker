13b969bc3b33c601760e50b88e8017c6baae3fa4
swoller22 <swoller22@gmail.com>
2021-12-01 17:52:23 -0500

    add Delete Account frontend

---
+++ client/static/js/account.js
@@ -0,0 +1,13 @@
+$(window).on('load', async () => {
+
+    axios.defaults.withCredentials = true
+    const result = await axios.get('http://localhost:5000/authentication/')
+    if (!result.data.isAuthenticated) {window.location.assign('../html/login.html')} 
+})
+
+$('#deleteAccount').click(async() => {
+
+    axios.defaults.withCredentials = true
+    const result = await axios.delete('http://localhost:5000/authentication')
+    console.log(result)
+})
